a82f674c5adc5045655bb2268e03e7f6c369e862
zach6585 <zmines@wesleyan.edu>
2021-05-16 22:43:10 -0400

    who knows how many commits I've done at this point

--- frontend/src/actions/users.js
+++ frontend/src/actions/users.js
@@ -1,7 +1,16 @@
 export const addUser = user => {
-    return {
-        type: 'ADD_USER',
-        user
+    return (dispatch) => {
+        dispatch({type: 'ADD_USER'});
+        fetch("localhost:3000/", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            }, 
+            body: JSON.stringify(user)
+        })
+        const data = await response.json()
+        
     }
 }
 
@@ -11,5 +20,12 @@ export function fetchUsers() {
         fetch("WILL INSERT PAGE WITH USERS HERE")
         .then(response => response.json())
         .then(users => console.log(users))
+    }
+}
+
+export function checkUser() {
+    return (dispatch) => {
+        dispatch({type: 'CHECK_USER'});
+        fetch("")
     }
 }
