c428d9444cb6355e6f749bbb207144144c277b9f
zach6585 <zmines@wesleyan.edu>
2021-05-16 21:58:49 -0400

    yeeye

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 
 import React, { Component } from 'react';
 import UserInput from './components/users/UserInput'
-import { BrowserRouter as Router,  Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router,  Route } from 'react-router-dom';
 import './App.css';
 import UserLogin from './components/users/UserLogin';
-
+import React, { Component } from 'react';
+import NavBar from '../components/NavBar';
+import Users from './containers/Users';
 
 class App extends Component {
   render(){
     return(
       <div className="App">
         <Router>
-            <Switch>]
+  
               <Route exact path="/register" component={UserInput} />
             <Route path="/login" component={UserLogin} />
             
-              {/* <Route exact path="/" component={HomePage} />   */}
-              <Redirect from="*" to="/" />
-              </Switch>
+
+            
           </Router>
         
       </div>
